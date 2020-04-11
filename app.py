d24cfed584c0c16ada36c087ca708626d13bfdd0
MamaBearGH <54992011+MamaBearGH@users.noreply.github.com>
2020-04-10 20:56:55 -0400

    Finished Mars Mission

---
+++ app.py
@@ -0,0 +1,30 @@
+import sys
+from flask
+import pymongo
+import scrape_mars
+import jQuery
+
+
+sys.setrecursionlimit(2000)
+app = Flask(__name__)
+
+client = pymongo.MongoClient()
+db = client.mars_db
+collection = db.mars_facts
+
+@app.route('/scrape')
+def scrape():
+    mars = scrape_mars.scrape()
+    print("\n\n\n")
+
+    db.mars_facts.insert_one(mars)
+    return "Some scrapped data"
+
+@app.route("/")
+def home():
+    mars = list(db.mars_facts.find())
+    print(mars)
+    return render_template("index.html", mars = mars)
+
+if __name__ == "__main__":
+    app.run(debug=True)
